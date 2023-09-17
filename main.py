26aa21cc04f654c4643108b26348b65bb28523bc
tikhon <tikhon.radk@gmail.com>
2023-09-17 17:23:04 +0300

    some fixe, neded to fill miss cabins

--- main.py
+++ main.py
@@ -17,9 +17,42 @@ def get_data() -> pd.DataFrame:
     test["type"] = 1
     all_data = pd.concat([train, test], axis=0)
 
-    all_data[["FirstName", "Surname"]] = all_data["Name"].str.split(" ", n=1, expand=True)
-    all_data["Siblings"] = all_data.groupby(["Surname", "Cabin"]).transform("size")
+    # fill 0 where CryoSleep is True
+    mask = all_data["CryoSleep"].isna()
+    all_data.loc[mask, "CryoSleep"] = False
+    columns_to_set_zero = ["RoomService", "FoodCourt", "ShoppingMall", "Spa", "VRDeck"]
+    all_data.loc[all_data["CryoSleep"], columns_to_set_zero] = 0
+    all_data.loc[mask, "CryoSleep"] = None
 
+    # From graph: I can set some HomePlanet and Destination by deck value
+    all_data[["deck", "num", "side"]] = all_data["Cabin"].str.split("/", n=2, expand=True)
+
+    con = (all_data["deck"].isin(["C", "T", "B", "A"])) & (all_data["HomePlanet"].isna())
+    all_data.loc[con, "HomePlanet"] = "Europa"
+    all_data.loc[(all_data["deck"].isin(["G"])) & (all_data["HomePlanet"].isna()), "HomePlanet"] = "Earth"
+
+    con = (all_data["deck"].isin(["F", "E", "T"])) & (all_data["Destination"].isna())
+    all_data.loc[con, "Destination"] = "TRAPPIST-1e"
+
+    all_data.loc[all_data["HomePlanet"].isna(), "HomePlanet"] = "Earth"
+    all_data.loc[all_data["Destination"].isna(), "Destination"] = "TRAPPIST-1e"
+    # all_data.loc[all_data["deck"].isna(), "deck"] = "G"
+
+    all_data[["GroupId", "PassengerNum"]] = all_data["PassengerId"].str.split("_", n=1, expand=True)
+    all_data["GroupId"] = all_data["GroupId"].astype(int)
+    all_data["PassengerNum"] = all_data["PassengerNum"].astype(int)
+    all_data["GroupSize"] = all_data.groupby("GroupId").transform("size")
+
+    print(all_data.head(20))
+    print(all_data.isna().sum())
+
+    # plot.age_to_transported(all_data.dropna())
+
+    # siblings by Surname
+    all_data[["FirstName", "Surname"]] = all_data["Name"].str.split(" ", n=1, expand=True)
+    all_data["Siblings"] = all_data.groupby(["Surname", "GroupId"]).transform("size")
+
+    # set gender
     n_to_gen_dict = name_to_gender.get()
     n_to_gen_dict.update({None: None})
     all_data["Gender"] = None
@@ -28,7 +61,6 @@ def get_data() -> pd.DataFrame:
     mask = all_data["Gender"].isna()
     d = gender.Detector()
     all_data.loc[mask, "Gender"] = all_data.loc[mask, "FirstName"].map(d.get_gender)
-    mask = all_data["Gender"].isin(["unknown", "male", "female", "mostly_male", "mostly_female", "andy"])
     all_data["Gender"] = all_data["Gender"].map({
         "unknown": None,  # 439
         "male": "male",  # 8206
@@ -37,15 +69,6 @@ def get_data() -> pd.DataFrame:
         "mostly_female": "female",  # 41
         "andy": "female"  # 107
     })
-
-    all_data[["GroupId", "PassengerNum"]] = all_data["PassengerId"].str.split("_", n=1, expand=True)
-    all_data["GroupId"] = all_data["GroupId"].astype(int)
-    all_data["PassengerNum"] = all_data["PassengerNum"].astype(int)
-    all_data["GroupSize"] = all_data.groupby("GroupId").transform("size")
-
-    all_data[["deck", "num", "side"]] = all_data["Cabin"].str.split("/", n=2, expand=True)
-
-    # plot.age_to_transported(all_data.dropna())
 
     bins = [0, 1, 4, 12, 17, 42, float("inf")]
     labels = ["0", "1-4", "5-12", "13-17", "18-42", "43+"]
--- plot.py
+++ plot.py
@@ -2,29 +2,54 @@ import plotly.graph_objects as go
 
 
 def age_to_transported(df):
-    age_transported_counts = df.groupby('Age')['Transported'].sum().reset_index()
+    df["side"] = df["side"].map({"P": 0, "S": 1})
+    age_transported_counts = df.groupby('HomePlanet')['side'].sum().reset_index()
     fig = go.Figure()
     fig.add_trace(
         go.Bar(
-            x=age_transported_counts['Age'],
-            y=age_transported_counts['Transported'],
-            name='Transported'
+            x=age_transported_counts['HomePlanet'],
+            y=age_transported_counts['side'],
+            name='side 1'
         )
     )
 
-    age_not_transported_counts = df.groupby('Age')['Transported'].count() - age_transported_counts['Transported']
+    age_not_transported_counts = df.groupby('HomePlanet')['side'].count()
     fig.add_trace(
         go.Bar(
-            x=age_transported_counts['Age'],
+            x=age_transported_counts['HomePlanet'],
             y=age_not_transported_counts,
-            name='Not Transported'
+            name='side 0'
         )
     )
     fig.update_layout(
-        xaxis_title='Age',
-        yaxis_title='Number of Individuals',
-        title='Transported and Not Transported Individuals by Age',
+        xaxis_title='home planet',
+        yaxis_title='side count',
+        title='planet to side',
         template="plotly_dark"  # Add this line for a black theme
     )
 
+    fig.show()
+
+
+def plot_count(data):
+
+
+    # Group the filtered data by 'HomePlanet' and count the number of people from each planet
+    planet_counts = data['deck'].value_counts()
+
+    # Create a bar chart
+    fig = go.Figure(go.Bar(
+        x=planet_counts.index,
+        y=planet_counts.values,
+    ))
+
+    # Customize the layout
+    fig.update_layout(
+        title='Distribution of People from Each Planet',
+        xaxis_title='Planet',
+        yaxis_title='Number of People',
+        template="plotly_dark"
+    )
+
+    # Show the plot
     fig.show()
